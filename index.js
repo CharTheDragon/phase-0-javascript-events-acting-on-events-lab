ac07bd986d4ad9649e329a4d360d28a16d37e7a0
Nathaniel Swan <cygnet1.999@gmail.com>
2023-04-15 17:21:03 -0400

    this is fun

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace('px','');
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 3}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const right = parseInt(leftNumbers, 10);
+
+    if (right < 360) {
+        dodger.style.left = `${right +3}px`;
+    }
+}
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    }
+    if (e.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+});
